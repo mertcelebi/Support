67801191a76f885a668be11f414b941299a86188
Graham Kaemmer <graham.kaemmer@yale.edu>
2015-06-23 22:56:10 -0400

    Tell express to listen on 0.0.0.0

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static('build'));
 
-var server = app.listen(4000, function () {
+var server = app.listen(4000, '0.0.0.0', function () {
   console.log("Server listening on port 4000")
 });
